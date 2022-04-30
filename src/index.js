0a40dffaa454dfe29b9602b40f56594735e20ee9
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-04-30 19:06:05 +0100

    Select the frame work.

--- src/index.js
+++ src/index.js
@@ -58,4 +58,90 @@ const renameFiles = {
 async function initialise() {
   let targetDir = argv._[0];
   let template = argv.template || argv.t;
+
+  const defaultProjectName = !targetDir ? "docusaurus-project" : targetDir;
+  let result = {};
+
+  try {
+    result = await prompts(
+      [
+        {
+          type: targetDir ? null : "text",
+          name: "projectName",
+          message: reset("Project name:"),
+          initial: defaultProjectName,
+          onState: (state) =>
+            (targetDir = state.value.trim() || defaultProjectName),
+        },
+        {
+          type: () =>
+            !fs.existsSync(targetDir) || isEmpty(targetDir) ? null : "confirm",
+          name: "overwrite",
+          message: () =>
+            (targetDir === "."
+              ? "Current directory"
+              : `Target directory "${targetDir}"`) +
+            ` is not empty. Remove existing files and continue?`,
+        },
+        {
+          type: (_, { overwrite } = {}) => {
+            if (overwrite === false) {
+              throw new Error(red("✖") + " Operation cancelled");
+            }
+            return null;
+          },
+          name: "overwriteChecker",
+        },
+        {
+          type: () => (isValidPackageName(targetDir) ? null : "text"),
+          name: "packageName",
+          message: reset("Package name:"),
+          initial: () => toValidPackageName(targetDir),
+          validate: (dir) =>
+            isValidPackageName(dir) || "Invalid package.json name",
+        },
+        {
+          type: template && TEMPLATES.includes(template) ? null : "select",
+          name: "framework",
+          message:
+            typeof template === "string" && !TEMPLATES.includes(template)
+              ? reset(
+                  `"${template}" isn't a valid template. Please choose from below: `,
+                )
+              : reset("Select a framework:"),
+          initial: 0,
+          choices: FRAMEWORKS.map((framework) => {
+            const frameworkColor = framework.color;
+            return {
+              title: frameworkColor(framework.name),
+              value: framework,
+            };
+          }),
+        },
+        {
+          type: (framework) =>
+            framework && framework.variants ? "select" : null,
+          name: "variant",
+          message: reset("Select a variant:"),
+          // @ts-ignore
+          choices: (framework) =>
+            framework.variants.map((variant) => {
+              const variantColor = variant.color;
+              return {
+                title: variantColor(variant.name),
+                value: variant.name,
+              };
+            }),
+        },
+      ],
+      {
+        onCancel: () => {
+          throw new Error(red("✖") + " Operation cancelled");
+        },
+      },
+    );
+  } catch (cancelled) {
+    console.log(cancelled.message);
+    return;
+  }
 }
