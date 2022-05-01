30002d05322a7c92d6c886e71bd58916ad928224
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-05-01 10:38:15 +0100

    Finish Code

--- index.js
+++ index.js
@@ -4,17 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const argv = require("minimist")(process.argv.slice(2), { string: ["_"] });
 const prompts = require("prompts");
-
-const {
-  yellow,
-  green,
-  cyan,
-  blue,
-  magenta,
-  lightRed,
-  red,
-  reset,
-} = require("kolorist");
+const { yellow, blue, red, reset } = require("kolorist");
 
 const cwd = process.cwd();
 
@@ -24,9 +14,14 @@ const frameWorks = [
     colour: yellow,
     variants: [
       {
-        name: "javascript",
-        display: "JavaScript",
+        name: "classic",
+        display: "Classic",
         colour: yellow,
+      },
+      {
+        name: "facebook",
+        display: "Facebook",
+        colour: blue,
       },
     ],
   },
@@ -36,30 +31,27 @@ const frameWorks = [
     variants: [
       {
         name: "typescript",
+        display: "Typescript",
         colour: blue,
-        display: "TypeScript",
       },
     ],
   },
 ];
 
-const templates = frameWorks
-  .map(
-    (frameWork) =>
-      (frameWork.variants && frameWork.variants.map((v) => v.name)) ||
-      frameWork.name,
-  )
+const TEMPLATES = frameWorks
+  .map((f) => (f.variants && f.variants.map((v) => v.name)) || [f.name])
   .reduce((a, b) => a.concat(b), []);
 
 const renameFiles = {
   _gitignore: ".gitignore",
 };
 
-async function initialise() {
+async function init() {
   let targetDir = argv._[0];
   let template = argv.template || argv.t;
 
   const defaultProjectName = !targetDir ? "docusaurus-project" : targetDir;
+
   let result = {};
 
   try {
@@ -111,9 +103,9 @@ async function initialise() {
               : reset("Select a framework:"),
           initial: 0,
           choices: frameWorks.map((framework) => {
-            const frameworkColour = framework.colour;
+            const frameworkColor = framework.colour;
             return {
-              title: frameworkColour(framework.name),
+              title: frameworkColor(framework.name),
               value: framework,
             };
           }),
@@ -123,12 +115,11 @@ async function initialise() {
             framework && framework.variants ? "select" : null,
           name: "variant",
           message: reset("Select a variant:"),
-          // @ts-ignore
           choices: (framework) =>
             framework.variants.map((variant) => {
-              const variantColour = variant.colour;
+              const variantColor = variant.colour;
               return {
-                title: variantColour(variant.name),
+                title: variantColor(variant.name),
                 value: variant.name,
               };
             }),
@@ -144,40 +135,64 @@ async function initialise() {
     console.log(cancelled.message);
     return;
   }
-}
 
-const { frameWork, overwrite, packageName, variant } = result;
-const root = path.join(cwd, targetDir);
+  const { framework, overwrite, packageName, variant } = result;
 
-if (overwrite) {
-  emptyDir(root);
-} else if (!fs.existsSync(root)) {
-  fs.mkdirSync(root);
-}
+  const root = path.join(cwd, targetDir);
 
-template = variant || frameWork || template;
-
-console.log(`\nScaffolding project in ${root}...`);
-
-const templateDir = path.join(__dirname, `template-${template}`);
-
-const write = (file, content) => {
-  const targetPath = renameFiles[file]
-    ? path.join(root, renameFiles[file])
-    : path.join(root, file);
-  if (content) {
-    fs.writeFileSync(targetPath, content);
-  } else {
-    copy(path.join(templateDir, file), targetPath);
+  if (overwrite) {
+    emptyDir(root);
+  } else if (!fs.existsSync(root)) {
+    fs.mkdirSync(root);
   }
-};
 
-const files = fs.readdirSync(templateDir);
-for (const file of files.filter((file) => file !== "package.json")) {
-  write(file);
+  template = variant || framework || template;
+
+  console.log(`\nScaffolding project in ${root}...`);
+
+  const templateDir = path.join(__dirname, `template-${template}`);
+
+  const write = (file, content) => {
+    const targetPath = renameFiles[file]
+      ? path.join(root, renameFiles[file])
+      : path.join(root, file);
+    if (content) {
+      fs.writeFileSync(targetPath, content);
+    } else {
+      copy(path.join(templateDir, file), targetPath);
+    }
+  };
+
+  const files = fs.readdirSync(templateDir);
+  for (const file of files.filter((f) => f !== "package.json")) {
+    write(file);
+  }
+
+  const pkg = require(path.join(templateDir, `package.json`));
+
+  pkg.name = packageName || targetDir;
+
+  write("package.json", JSON.stringify(pkg, null, 2));
+
+  const pkgInfo = pkgFromUserAgent(process.env.npm_config_user_agent);
+  const pkgManager = pkgInfo ? pkgInfo.name : "npm";
+
+  console.log(`\nDone. Now run:\n`);
+  if (root !== cwd) {
+    console.log(`  cd ${path.relative(cwd, root)}`);
+  }
+  switch (pkgManager) {
+    case "yarn":
+      console.log("  yarn");
+      console.log("  yarn start");
+      break;
+    default:
+      console.log(`  ${pkgManager} install`);
+      console.log(`  ${pkgManager} run start`);
+      break;
+  }
+  console.log();
 }
-
-const pkg = require(path.join(templateDir, "package.json"));
 
 function copy(src, dest) {
   const stat = fs.statSync(src);
@@ -186,6 +201,35 @@ function copy(src, dest) {
   } else {
     fs.copyFileSync(src, dest);
   }
+}
+
+function isValidPackageName(projectName) {
+  return /^(?:@[a-z0-9-*~][a-z0-9-*._~]*\/)?[a-z0-9-~][a-z0-9-._~]*$/.test(
+    projectName,
+  );
+}
+
+function toValidPackageName(projectName) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/^[._]/, "")
+    .replace(/[^a-z0-9-~]+/g, "-");
+}
+
+function copyDir(srcDir, destDir) {
+  fs.mkdirSync(destDir, { recursive: true });
+  for (const file of fs.readdirSync(srcDir)) {
+    const srcFile = path.resolve(srcDir, file);
+    const destFile = path.resolve(destDir, file);
+    copy(srcFile, destFile);
+  }
+}
+
+function isEmpty(path) {
+  const files = fs.readdirSync(path);
+  return files.length === 0 || (files.length === 1 && files[0] === ".git");
 }
 
 function emptyDir(dir) {
@@ -202,3 +246,21 @@ function emptyDir(dir) {
     }
   }
 }
+
+/**
+ * @param {string | undefined} userAgent process.env.npm_config_user_agent
+ * @returns object | undefined
+ */
+function pkgFromUserAgent(userAgent) {
+  if (!userAgent) return undefined;
+  const pkgSpec = userAgent.split(" ")[0];
+  const pkgSpecArr = pkgSpec.split("/");
+  return {
+    name: pkgSpecArr[0],
+    version: pkgSpecArr[1],
+  };
+}
+
+init().catch((e) => {
+  console.error(e);
+});
